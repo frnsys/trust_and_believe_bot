92462255f2fe3b5e6581e1ce88f1c85de99b71cf
Francis Tseng <f+accounts@frnsys.com>
2017-04-29 17:49:18 -0400

    added data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import pandas as pd
 from time import sleep
 
-adj_mat = pd.read_csv('data.csv', index_col='id')
+adj_mat = pd.read_csv('adj_mat.csv', index_col='id')
 
 arena.access_token = config.ARENA_TOKEN
 
@@ -18,7 +18,7 @@ patterns = [
 ]
 
 
-def watch_chan(chan, on_new_block, poll_every=1, prob=0.02, replay=True):
+def watch_chan(chan, on_new_block, poll_every=1, prob=0.04, replay=True):
     chan = arena.channels.channel(chan)
     blocks, _ = chan.contents()
 
@@ -38,26 +38,28 @@ def watch_chan(chan, on_new_block, poll_every=1, prob=0.02, replay=True):
                 on_new_block(b)
             blocks = blocks_
         else:
-            if random.random() < prob/poll_every:
+            if random.random() < prob/poll_every and blocks:
                 on_new_block(blocks[-1])
         sleep(poll_every)
 
 
 def say(block):
     pattern = random.choice(patterns)
-    id = get_similar(str(block.id))
+    id = get_similar(block.id)
     if id is not None:
         para = sample_text(id)
-        para = '\n\n> {}\n'.format(para)
-        statement = pattern + para
-        print(statement)
-        print('---')
+        if para is not None:
+            print('\n')
+            para = '\n\n{}\n'.format(para)
+            statement = pattern + para
+            print(statement)
+            print('---')
 
 
 def get_similar(id, choices=3):
     """returns the most similar id for the specified id"""
     try:
-        choices = adj_mat.loc[id].sort_values(ascending=False).index[1:1+choices]
+        choices = adj_mat.loc[id].sort_values(ascending=False).index[0:choices]
         return random.choice(choices)
     except KeyError:
         return None
@@ -66,9 +68,16 @@ def get_similar(id, choices=3):
 def sample_text(id):
     """returns a random paragraph from block text of the specified id"""
     with open('texts/{}.txt'.format(id)) as f:
-        text = f.read()
+        text = f.read().strip()
+    if not text:
+        return None
     paras = [p for p in text.split('\n') if p.strip()]
-    return random.choice(paras)
+    sents = []
+    for p in paras:
+        sents += [s.strip() for s in p.split('. ')]
+    sents = random.sample(sents, min(len(sents), 3))
+    sents = ['> {}'.format(s) for s in sents]
+    return '\n'.join(sents)
 
 
 if __name__ == '__main__':
@@ -76,6 +85,6 @@ if __name__ == '__main__':
         say(block)
 
     try:
-        watch_chan(config.WATCH_CHAN, on_new_block, replay=False)
+        watch_chan(config.WATCH_CHAN, on_new_block, replay=True)
     except (KeyboardInterrupt, SystemExit):
         pass
